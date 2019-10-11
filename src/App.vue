be47526b66a2384a4e4eaa8a7dc6e1cbb2c26ea6
Sarah Grant <sarah@ghijk.co.uk>
2019-10-11 18:45:28 +0100

    friday night working on sorting hat form

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template lang="html">
   <div class="main-container">
     <h1>Harry Potter</h1>
-    <h3>{{sortingHat}}</h3>
+    <sorting-hat></sorting-hat>
     <!-- <p>{{characters}}</p> -->
     <!-- <p>{{spells}}</p> -->
   </div>
 </template>
 
 <script>
+import SortingHat from './components/sortingHat'
 export default {
   name: 'app',
   data(){
     return{
-      sortingHat: "",
       characters: [],
       spells: []
     };
   },
+  components: {
+    'sorting-hat': SortingHat
+  },
 
   mounted(){
-    fetch("https://www.potterapi.com/v1/sortinghat")
-    .then(response => response.json())
-    .then(sortingHat => this.sortingHat = sortingHat)
-
     fetch("https://www.potterapi.com/v1/characters?key=$2a$10$wmc1QkABdgEulOcSMTdYz.qQgGJyMBJXVNoXNJu5428XykYmJCHW6")
     .then(response => response.json())
     .then(characters => this.characters = characters)
---
+++ src/components/sortingHat.vue
@@ -0,0 +1,44 @@
+<template lang="html">
+  <div class="">
+    <form>
+      <input v-model="name" placeholder="Name" type="text" name="" value="">
+      <p>Pick your pet: </p>
+      <input v-model="picked" type="radio" name="pet" value="Cat"><label for="Cat">Cat</label><br>
+      <input v-model="picked" type="radio" name="pet" value="Owl"><label for="Owl">Owl</label><br>
+      <input v-model="picked" type="radio" name="pet" value="Newt"><label for="Newt">Newt</label><br>
+      <input v-model="picked" type="radio" name="pet" value="Rat"><label for="Rat">Rat</label><br>
+      <br>
+      <button @click="sortIntoHouse" type="button" name="button">Sort into House</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sorting-hat',
+
+  data(){
+    return{
+      sortingHat: "",
+      personsHouse: []
+    }
+  },
+  method:{
+    sortIntoHouse(){
+
+    }
+
+  },
+
+  mounted(){
+    fetch("https://www.potterapi.com/v1/sortinghat")
+    .then(response => response.json())
+    .then(sortingHat => this.sortingHat = sortingHat)
+  }
+}
+
+
+</script>
+
+<style lang="css" scoped>
+</style>
